175883b892da5ba404bbc9726e95b3283b244ea7
Srujan <Srujan560@gmail.com>
2022-09-11 22:55:53 -0400

    add some components and funcation to the site

--- study-easy-react/src/App.js
+++ study-easy-react/src/App.js
@@ -4,7 +4,9 @@ import {BrowserRouter,Route,Routes,Link} from "react-router-dom"
 import About from "./Pages/About"
 import Home from "./Pages/Home"
 import Extra from './Pages/Extra';
+import {Country} from "./Pages/Country";
 import {University} from './Pages/University';
+
 
 function App() {
   return (
@@ -14,13 +16,17 @@ function App() {
         <li><Link to ="/">Home</Link></li>
         <li><Link to = "/About">About</Link></li>
         <li><Link to = "/Extra">Extra</Link></li>
-        <li><Link to = "/Univ">University (put country)</Link></li>
+        <li><Link to = "/Country">Country</Link></li>
+        <li><Link to = "/Univ">University (Only testing (remove me later))</Link></li>
+        
       </ul>
       <Routes>
         <Route path="/" exact element={<Home/>} />
         <Route path = "/About" exact element={<About/>}/>
         <Route path ="/Extra" exact element = {<Extra/>} />
+        <Route path ="/Country" exact element = {<Country/>}/>
         <Route path="/Univ" exact element = {<University/>} />
+        
       </Routes>
     </BrowserRouter>
    
--- study-easy-react/src/Pages/University.js
+++ study-easy-react/src/Pages/University.js
@@ -1,8 +1,41 @@
 export const University = (data) =>{
+  const country = data.country
+    if (country==='USA'){
+      return(
+        <div>
+          <h1>Welcome to USA </h1>
+          <College collegeName="UAblany" city="Albany" zipCode={12345} url= "https://albany.edu/"/>
+          <College collegeName= "ULosAngles" city = "Los Angles" zipCode={99993} url ="https://www.programiz.com/python-programming/time/sleep"/>
+        </div>
+      );
+    }
+    if(country==='IND'){
+     return( 
+     <div>
+        <h1>Welcome to India</h1>
+        <College collegeName="H.Tech Ashoka" city ="New Delhi" zipCode={9091321} url= "https://fastapi.io"/>
+        <College collegeName="Darius" city ="Pune" zipCode={19123321} url= "https://www.robinwieruch.de/react-dropdown/"/>
+    </div>)
+    }
+    if(country==='CHI'){
+      return(
+        <div>
+          <h1>Welcome to China</h1>
+          <College collegeName="Peking University" city ="Beijing" zipCode={443211} url= "https://reactjs.org/docs/conditional-rendering.html"/>
+          <College collegeName="Han-Wu" city ="Beijing" zipCode={443200} url= "https://reactjs.org/docs/conditional-rendering.html"/>
+
+        </div>)
+    }
+    if(country==='None'){
+      return(<div>
+        <h4>Please Pick a Country</h4>
+      </div>)
+    }
+    
     return(
         <div>
-            <College collegeName="UAblany" city="Albany" zipCode={12345} url= "https://albany.edu/"/>
-            <College collegeName= "ULosAngles" city = "Los Angles" zipCode={99993} url ="https://www.programiz.com/python-programming/time/sleep"/>
+          <h4>{data.country}</h4>
+            
         </div>);
 };
 
@@ -18,12 +51,12 @@ export const University = (data) =>{
 //     )
 //   }
 // }
-const College = (props) =>{
+const College = (aarops) =>{
     return (
       <div>
-        <h2>{props.collegeName}</h2>
-        <h3>{props.city}, {props.zipCode}</h3>
-        <h4><a href={props.url}>Click for {props.collegeName}</a></h4>
+        <h2>{aarops.collegeName}</h2>
+        <h3>{aarops.city}, {aarops.zipCode}</h3>
+        <h4><a href={aarops.url}>Click for {aarops.collegeName}</a></h4>
       </div>)
 }
 
