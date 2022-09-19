14b52b3dd4eaeb1532189d681ebd2e9e056a9fce
Abid Khawaja <akhawaja@albany.edu>
2022-09-19 14:30:08 -0400

    saved

--- study-easy-react/src/App.js
+++ study-easy-react/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+// import ReactDOM from 'react-dom/client'
+import {BrowserRouter,Route,Routes,Link} from "react-router-dom"
+import About from "./Pages/About"
+import Home from "./Pages/Home"
+import Extra from './Pages/Extra';
+import {Country} from "./Pages/Country";
+import {University} from './Pages/University';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+      <ul>
+        <li><Link to ="/">Home</Link></li>
+        <li><Link to = "/About">About</Link></li>
+        <li><Link to = "/Extra">Extra</Link></li>
+        <li><Link to = "/Country">Country</Link></li>
+        <li><Link to = "/Univ">University (Only testing (remove me later))</Link></li>
+        
+      </ul>
+      <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path = "/About" exact element={<About/>}/>
+        <Route path ="/Extra" exact element = {<Extra/>} />
+        <Route path ="/Country" exact element = {<Country/>}/>
+        <Route path="/Univ" exact element = {<University/>} />
+        
+      </Routes>
+    </BrowserRouter>
+   
   );
 }
 
---
+++ study-easy-react/src/Pages/University.js
@@ -0,0 +1,62 @@
+export const University = (data) =>{
+  const country = data.country
+    if (country==='USA'){
+      return(
+        <div>
+          <h1>Welcome to USA </h1>
+          <College collegeName="UAblany" city="Albany" zipCode={12345} url= "https://albany.edu/"/>
+          <College collegeName= "ULosAngles" city = "Los Angles" zipCode={99993} url ="https://www.programiz.com/python-programming/time/sleep"/>
+        </div>
+      );
+    }
+    if(country==='IND'){
+     return( 
+     <div>
+        <h1>Welcome to India</h1>
+        <College collegeName="H.Tech Ashoka" city ="New Delhi" zipCode={9091321} url= "https://fastapi.io"/>
+        <College collegeName="Darius" city ="Pune" zipCode={19123321} url= "https://www.robinwieruch.de/react-dropdown/"/>
+    </div>)
+    }
+    if(country==='CHI'){
+      return(
+        <div>
+          <h1>Welcome to China</h1>
+          <College collegeName="Peking University" city ="Beijing" zipCode={443211} url= "https://reactjs.org/docs/conditional-rendering.html"/>
+          <College collegeName="Ming vu university " city ="Beijing" zipCode={443200} url= "https://reactjs.org/docs/conditional-rendering.html"/>
+
+        </div>)
+    }
+    if(country==='None'){
+      return(<div>
+        <h4>Please Pick a Country</h4>
+      </div>)
+    }
+    
+    return(
+        <div>
+          <h4>{data.country}</h4>
+            
+        </div>);
+};
+
+
+// export default class university extends Component {
+//   render() {
+//     return (
+//       <div>university
+//         <College collegeName="UAblany" city="Albany" zipCode={12345} url= "https://albany.edu/"/>
+//         <College collegeName= "ULosAngles" city = "Los Angles" zipCode={99993} url ="https://www.programiz.com/python-programming/time/sleep"/>
+//       </div>
+      
+//     )
+//   }
+// }
+const College = (aarops) =>{
+    return (
+      <div>
+        <h2>{aarops.collegeName}</h2>
+        <h3>{aarops.city}, {aarops.zipCode}</h3>
+        <h4><a href={aarops.url}>Click for {aarops.collegeName}</a></h4>
+      </div>)
+}
+
