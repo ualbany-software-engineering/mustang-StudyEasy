866a45f1381586bfe21ddabc2012c2e7cc1004d5
Abid Khawaja <akhawaja@albany.edu>
2022-09-24 19:39:52 -0400

    country page (not to be used, using different version).

--- study-easy-react/src/Pages/Country.js
+++ study-easy-react/src/Pages/Country.js
@@ -10,26 +10,31 @@ import UKflag from "../Pages/images/UKflag.png";
 import USAflag from "../Pages/images/USAflag.png";
 
 export const Country = () => {
-    var countryOptions = [
-        {value: 'USA', label: "United States of America"},
-        {value: 'IND', label: "India"},
-        {value: 'CHI', label: "China"},
-        {value: 'CAN', label: "Canada"},
-        {value: 'UK', label: "United Kingdom"},
-        {value: 'AUS', label: "Australia"},
-        {value :'None', label: 'None'}
-    ];
-    const [selectedCountry,updateData]= useState("Please pick a Country");
+    
+    const [selectedCountry,updateData]= useState(false);
     const update = (e) =>{updateData(e.value)}
     const imageStyle = {width: '350px', height: '225px' };
     return (
         <div>
             <h1>Welcome to StudyEasy</h1>
-            <button><img style={imageStyle} src={canadaflag}  /></button>
-            <button><img style={imageStyle} src={chinaflag} /></button>
-            <button><img style={imageStyle} src={indiaflag} /></button>
-            <button><img style={imageStyle} src={UKflag} /></button>
-            <button><img style={imageStyle} src={USAflag} /></button> 
-            <button><img style={imageStyle} src={australiaflag} /></button>
+            <button></button>
+            <h1>Canada</h1>
+            <button onClick={()=>{updateData(!selectedCountry)}}><img style={imageStyle} src={canadaflag}  /></button>
+            {selectedCountry ? <University country = 'CAN'/>: null}
+            <h1>China</h1>
+            <button onClick={()=>{updateData(!selectedCountry)}}><img style={imageStyle} src={chinaflag} /></button>
+            {selectedCountry ? <University country = 'CHI'/>: null}
+            <h1>India</h1>
+            <button onClick={()=>{updateData(!selectedCountry)}}><img style={imageStyle} src={indiaflag} /></button>
+            {selectedCountry ? <University country = 'IND'/>: null}
+            <h1>United Kingdom</h1>
+            <button onClick={()=>{updateData(!selectedCountry)}}><img style={imageStyle} src={UKflag} /></button>
+            {selectedCountry ? <University country = 'UK'/>: null}
+            <h1>USA</h1>
+            <button onClick={()=>{updateData(!selectedCountry)}}><img style={imageStyle} src={USAflag} /></button> 
+            {selectedCountry ? <University country = 'USA'/>: null}
+            <h1>Australia</h1>
+            <button onClick={()=>{updateData(!selectedCountry)}}><img style={imageStyle} src={australiaflag} /></button>
+            {selectedCountry ? <University country = 'AUS'/>: null}
         </div>);
 };
