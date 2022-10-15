f3b6f370f4b609ae157b5e114c3192899ace8e0f
Srujan <Srujan560@gmail.com>
2022-10-14 22:17:19 -0400

    Add form with Dynamic Form Fields

--- study-easy-react/src/countrypages/addMajors.js
+++ study-easy-react/src/countrypages/addMajors.js
@@ -3,49 +3,75 @@ import {Form, Formik, Field, FieldArray} from 'formik';
 import Grid from '@mui/material/Grid';
 import {object, number, string} from 'yup'
 import { async } from "@firebase/util";
-import { TextField, Typography } from "@mui/material";
+import {Typography } from "@mui/material";
+import TextField from '@mui/material/TextField';
+// import { TextField } from 'formik-material-ui';
+
 import {db} from "../firbase-config";
-import {collection, Firestore, getDocs} from "firebase/firestore";
+import {collection, addDoc} from "firebase/firestore";
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 
 
 export default function AddMajors () {
   return (
-    <Container variant="contained" backgroundColor="white" > 
-        <Formik initialValues={{name:'',
-              undergrad:[],
-              grad:[]}}
+    <Container variant="contained"  > 
+        <Formik initialValues={{majorName:'',
+              undergrad:[{courseName:'',dispersion:""}],
+              grad:[{courseName:'',dispersion:""}]}}
               validationSchema={object({
-                name:string("Please enter the study Flied").required().min(2)
+                majorName:string("Please enter the study Flied").required().min(2)
             })}
-              onSubmit={()=> {async (values =>{
-                console.log('My Values from Colleges .js ', values)
-                return new Promise (res => setTimeout(res, 3000))
-              })}
+              onSubmit={async (values) => {
+                console.log('My Values from addMajors .js ', values)
+                return new Promise ((res) => setTimeout(res, 3000))
+              }
               }>
               {({values,errors, isSubmitting})=>(
                 <Form autoComplete="off">
-                <Grid container direction="column" spacing={2}>
+                <Grid container direction="column" spacing={4}>
                   <Grid item>
-                    <Field name="name" />
+                    <Field  name="majorName" label="Field of Study" placeholder="Math" fullWidth />
+                    {/* <TextField id="majorName" name="majorName" label="Field of Study" fullWidth  onChange={values.majorName}/> */}
                   </Grid>
-                  <Grid>
+                   
                     <FieldArray name="undergrad">
-                    {({push,remove}) => (
+                    {({push,remove,}) => (
                       <React.Fragment>
                         <Grid item> 
-                          <Typography variant="body2"></Typography>
+                          <Typography variant="body2">Add Class For UnderGrad</Typography>
                         </Grid>
 
                         {values.undergrad.map((_val, ind) => (
-                            <Grid container item>
-                                <Grid item>
-                                <Field name={`undergrad.${ind}`}
-                                    componenet={TextField}
+                            <Grid container item spacing={2}>
+                                <Grid item >
+                                <Field name={`undergrad.${ind}.courseName`}
+                                    placeholder="mat 101"
                                 />
+                                </Grid>
 
-                            
+                                <Grid item xs={12}>
+                                    <Field name={`undergrad.${ind}.dispersion`}
+                                    placeholder="In this class student will learn How to add numbers"
+                                    width={10}
+                                />
+                               
+                                </Grid>
+                                {ind>0 &&
+                                    <Grid item>
+                                        <Button onClick={() => remove(ind)}
+                                            variant="contained"  color="error"
+                                        >remove</Button>
+                                    
+                                    </Grid>
+                                }   
+
+                                <Grid item>
+                                    <Button 
+                                    variant="contained" color="success"
+                                    onClick={() => push({courseName:'',dispersion:''})}>
+                                        ADD
+                                    </Button>
                                 </Grid>
                             </Grid>
                             
@@ -53,15 +79,56 @@ export default function AddMajors () {
                       </React.Fragment>
                     )}
                   </FieldArray>
-                  </Grid>
 
-                  <Grid>
-                    <FieldArray name="grad">
+                  <FieldArray name="grad">
+                    {({push,remove,}) => (
+                      <React.Fragment>
+                        <Grid item> 
+                          <Typography variant="body2">Add Class For Grad</Typography>
+                        </Grid>
 
-                    </FieldArray>
-                  </Grid>
+                        {values.grad.map((_val, ind) => (
+                            <Grid container item spacing={2}>
+                                <Grid item>
+                                <Field name={`grad.${ind}.courseName`}
+                                    placeholder="mat 518"
+                                />
+                                </Grid>
 
-                  <Grid>
+                                <Grid item>
+                                    <Field name={`grad.${ind}.dispersion`}
+                                    placeholder="In this class student will expiring DIp"
+                                    width={10}
+                                />
+                               
+                                </Grid>
+                                {ind>0 &&
+                                    <Grid item>
+                                        <Button onClick={() => remove(ind)}
+                                            variant="contained"  color="error"
+                                        >remove</Button>
+                                    
+                                    </Grid>
+                                }   
+
+                                <Grid item>
+                                    <Button 
+                                    variant="contained" color="success"
+                                    onClick={() => push({courseName:'',dispersion:''})}>
+                                        ADD
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                            
+                        ))}
+                      </React.Fragment>
+                    )}
+                  </FieldArray>
+                  
+
+
+                            <br/>
+                  <Grid item xs={12}>
                     <Button 
                       disabled={isSubmitting}
                       type="submit" 
@@ -71,6 +138,7 @@ export default function AddMajors () {
                       {isSubmitting? 'Submitting':'Submit'}
                       </Button>
                   </Grid>
+                  <br/>
                 </Grid>
                 <pre>{JSON.stringify({values,errors},null,4)}</pre>
                  </Form>
