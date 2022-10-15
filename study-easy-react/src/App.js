2e606bfccc7e77cfdfe0c3469367c51976e341e2
anudeep-17 <anudeepsai88@gmail.com>
2022-10-14 20:32:34 -0400

    edit

--- study-easy-react/src/App.js
+++ study-easy-react/src/App.js
@@ -1,20 +1,14 @@
-
 import './App.css';
-import Headertab from './designpages/Headertab';
 // import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import React from 'react';
-
-import AnimationRoutes from './components/AnimationRoutes';
-import Bodytab from './designpages/bodytab';
 
 function App() {
  
   return (
     <BrowserRouter basename = {process.env.PUBLIC_URL}>
-    <Headertab/>
     <div className="App">
-          <AnimationRoutes/>
+      
     </div>
     </BrowserRouter>
   );
--- study-easy-react/src/pages/CollegeDisplay.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const CollegeDisplay = () => {
-  return (
-    <div>
-        Hello world
-    </div>
-  )
-}
