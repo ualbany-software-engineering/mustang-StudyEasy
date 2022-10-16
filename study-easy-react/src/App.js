49a6738edafbcce01e2f262c75ef5bf3fe809b46
Srujan <Srujan560@gmail.com>
2022-10-15 21:40:06 -0400

    added admin update create and delte

--- study-easy-react/src/App.js
+++ study-easy-react/src/App.js
@@ -1,22 +1,20 @@
-
-import './App.css';
-import Headertab from './designpages/Headertab';
+import "./App.css";
+import Headertab from "./designpages/Headertab";
 // import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import React from 'react';
+import React from "react";
 
-import AnimationRoutes from './components/AnimationRoutes';
+import AnimationRoutes from "./components/AnimationRoutes";
 // import Bodytab from './designpages/bodytab';
-import './firbase-config'
+import "./firbase-config";
 
 function App() {
- 
   return (
-    <BrowserRouter basename = {process.env.PUBLIC_URL}>
-    <Headertab/>
-    <div className="App">
-          <AnimationRoutes/>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Headertab />
+      <div className="App">
+        <AnimationRoutes />
+      </div>
     </BrowserRouter>
   );
 }
