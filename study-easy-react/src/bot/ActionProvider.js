fae92d60e50259cb52d62a6b6ff5256029db3a34
ForumShah8 <shahforu00@gmail.com>
2022-10-15 20:28:19 -0400

    updates to the chatbot

--- study-easy-react/src/bot/ActionProvider.js
+++ study-easy-react/src/bot/ActionProvider.js
@@ -1,4 +1,6 @@
 import React from "react";
+import '../bot/custom.css';
+
 const ActionProvider = ({ createChatBotMessage, setState, children}) => {
     const handleHello = () => {
         const botMessage = createChatBotMessage('hi how can I help you?');
@@ -7,20 +9,27 @@ const ActionProvider = ({ createChatBotMessage, setState, children}) => {
             messages: [...prev.messages, botMessage], 
         }));
     };
+    const handleNull = () => {
+        const botMessage = createChatBotMessage('Sorry I was unable to find what you are looking for..');
+        setState((prev) => ({
+            ...prev,
+            messages: [...prev.messages, botMessage], 
+        }));
+    };
     const handleCountries = () => {
-        //const botMessage = createChatBotMessage('Here is a list of countries:USA \n INDIA \n CHINA \n CANADA \n UK \n AUSTRALIA');
-        const botMessage = createChatBotMessage( <>Here is the link to the countries: <br/> 
-        <a href="http://localhost:3000/mustang-StudyEasy/Univ/USA">USA</a> 
+        const botMessage = createChatBotMessage( <>Here is the link to the countries: 
+        <br/>
+        <a class="links" href="http://localhost:3000/mustang-StudyEasy/Univ/USA">List of Colleges in USA</a> 
         <br/> 
-        <a href="http://localhost:3000/mustang-StudyEasy/Univ/IND">INDIA</a>
+        <a class="links" href="http://localhost:3000/mustang-StudyEasy/Univ/IND">List of Colleges in INDIA</a>
         <br/>
-        <a href="http://localhost:3000/mustang-StudyEasy/Univ/CHI">CHINA</a>
+        <a class="links" href="http://localhost:3000/mustang-StudyEasy/Univ/CHI">List of Colleges in CHINA</a>
         <br/>
-        <a href="http://localhost:3000/mustang-StudyEasy/Univ/CAN">CANADA</a>
+        <a class="links" href="http://localhost:3000/mustang-StudyEasy/Univ/CAN">List of Colleges in CANADA</a>
         <br/>
-        <a href="http://localhost:3000/mustang-StudyEasy/Univ/UK">UK</a>
+        <a class="links" href="http://localhost:3000/mustang-StudyEasy/Univ/UK">List of Colleges in UK</a>
         <br/>
-        <a href="http://localhost:3000/mustang-StudyEasy/Univ/AUS">AUSTRALIA</a>
+        <a class="links" href="http://localhost:3000/mustang-StudyEasy/Univ/AUS">List of Colleges in AUSTRALIA</a>
         </>)
        
     
@@ -29,6 +38,39 @@ const ActionProvider = ({ createChatBotMessage, setState, children}) => {
             messages: [...prev.messages, botMessage], 
         }));
     };
+    const handleResources = () => {
+        const botMessage = createChatBotMessage(<>I have a list of study resources available for you: <br/>
+        <a href="http://localhost:3000/mustang-StudyEasy/gre" class="links">GRE Study Materials</a>
+        <br/>
+        <a href="http://localhost:3000/mustang-StudyEasy/ielts" class="links">IELTS Study Materials</a>
+        <br/>
+        <a href="http://localhost:3000/mustang-StudyEasy/gmat" class="links">GMAT Study Materials</a>
+        </>);
+        setState((prev) => ({
+            ...prev,
+            messages: [...prev.messages, botMessage], 
+        }));
+    };
+    const handleVisa = () => {
+        const botMessage = createChatBotMessage(<>You want to get Visa Information for which country? <br/>
+        <a href="http://localhost:3000/mustang-StudyEasy/usavisa" class="links">USA VISA INFORMATION</a>
+        <br/>
+        <a href="http://localhost:3000/mustang-StudyEasy/indiavisa" class="links">INDIAN VISA INFORMATION</a>
+        <br/>
+        <a href="http://localhost:3000/mustang-StudyEasy/chinavisa" class="links">CHINA VISA INFORMATION</a>
+        <br/>
+        <a href="http://localhost:3000/mustang-StudyEasy/canadavisa" class="links">CANADA VISA INFORMATION</a>
+        <br/>
+        <a href="http://localhost:3000/mustang-StudyEasy/ukvisa" class="links">UK VISA INFORMATION</a>
+        <br/>
+        <a href="http://localhost:3000/mustang-StudyEasy/canadavisa" class="links">AUSTRALIA VISA INFORMATION</a>
+        </>);
+        setState((prev) => ({
+            ...prev,
+            messages: [...prev.messages, botMessage], 
+        }));
+    };
+    
     return (
         <div>
             {React.Children.map(children, (child)=>{
@@ -36,6 +78,9 @@ const ActionProvider = ({ createChatBotMessage, setState, children}) => {
                     actions: {
                         handleHello,
                         handleCountries,
+                        handleResources,
+                        handleVisa, 
+                        handleNull
                     },
                 });
             })}
--- study-easy-react/src/bot/MessageParser.js
+++ study-easy-react/src/bot/MessageParser.js
@@ -5,10 +5,21 @@ const MessageParser = ({ children, actions}) => {
        // console.log('hi how can I help you?');
        actions.handleHello();
        } 
-       else if(message.includes('1')){
+       else if(message.includes('1') || message.toLowerCase().includes('countries')){
         
         actions.handleCountries();
+        }
+    else if(message.includes('2')){
+    
+        actions.handleResources();
         } 
+    else if(message.includes('3')){
+
+        actions.handleVisa();
+        } 
+    else {
+        actions.handleNull();
+    }
     };
     return (
         <div>
