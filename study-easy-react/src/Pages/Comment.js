1e0052e5791382edcfd34d1a700ebf6636bc1651
Suraj Goud Rachamalla <surajgoud36@gmail.com>
2022-10-19 21:26:53 -0400

    Added User reviews page

---
+++ study-easy-react/src/Pages/Comment.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import {useState,useEffect} from "react";
+import {db} from '../firbase-config';
+import pic from '../assets/userIcon.png';
+import '../styling/Comment.css';
+import { useNavigate } from "react-router-dom";
+import {collection,getDocs,addDoc,updateDoc,doc,deleteDoc} from 'firebase/firestore';
+function Comment() {
+    
+    const [users,setUsers]= useState([]);
+    const userCollectionRef=collection(db,"Comments");
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+        
+        navigate("/CreateComment");
+      };
+    
+      useEffect(()=>{
+        const getUsers = async()=>{
+          const data=await getDocs(userCollectionRef);
+          setUsers(data.docs.map((doc)=>({...doc.data(),id:doc.id})));
+        };
+        getUsers();
+      },[]);
+  return (
+    <>
+        <div className='zazu'>
+        <div className='theading'>
+            <span>Reviews</span>
+            <h1>Student's Saying</h1>
+        </div>
+        <div className='tboxC'>
+        {users.map((user)=>{
+            return(
+                
+                    <div className='tbox'>
+                        <div className='box-top'>
+                            <div className='profile'>
+                                <div className='profile-img'>
+                                    <img src={pic} alt="fafs"/>
+                                </div>
+                                <div className='name-user'>
+                                    <strong>{user.name}</strong>
+                                    <span> @{user.name} </span>
+                                </div>
+                            </div>
+                            <div className='reviews'></div>
+                        </div>
+                        <div className='client-comment'>
+                            <p>{user.age}</p>
+                        </div>
+                        
+                    </div>
+            
+                
+            );
+        })}
+
+        </div>
+        <button className='button' onClick={routeChange}>Write Review</button>;
+    </div>
+    
+    </>
+
+    
+  );
+}
+
+export default Comment
