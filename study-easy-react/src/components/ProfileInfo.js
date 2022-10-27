41a2f3b63c288f703d7704bc954bef253de0156b
Abid Khawaja <akhawaja@albany.edu>
2022-10-26 20:16:02 -0400

    fixed sign out function

--- study-easy-react/src/components/ProfileInfo.js
+++ study-easy-react/src/components/ProfileInfo.js
@@ -1,6 +1,8 @@
 import { auth, db } from '../firbase-config';
 import { collection, getDocs, updateDoc, doc, deleteDoc } from "firebase/firestore";
 import {useEffect, useState} from "react";
+import { getAuth, signOut } from 'firebase/auth';
+import { useNavigate } from "react-router-dom";
 
 export default function UserProfile(){
     const[getUser,setGetUser] = useState([])
@@ -14,6 +16,23 @@ export default function UserProfile(){
         }
         getUserInfo();
     },[])
+
+    let navigate = useNavigate();
+    const routeChange = () => {
+        let path = `/`;
+        navigate(path);
+    }
+
+    const SignOut = () => {
+        const auth = getAuth();
+        signOut(auth).then(() => {
+            alert("Successfully signed out")
+            routeChange()
+          }).catch((error) => {
+            const errorCode = error.code;
+            alert(errorCode)
+          });
+    }
     return (
         <div>
             <h1>userid={auth.currentUser?.uid}</h1>
@@ -24,6 +43,7 @@ export default function UserProfile(){
                             <h2>firstname = {e.Fname}</h2>
                             <h2>lastname = {e.Lname}</h2>
                             <h2>phonenumber = {e.PhoneNumber}</h2>
+                            <button variant="contained" onClick={SignOut}>Sign Out</button>
                         </div>
                     )
                 }
