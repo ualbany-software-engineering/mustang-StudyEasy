de418d10ecc249dacfae341846f3e89d719b874f
Abid Khawaja <akhawaja@albany.edu>
2022-11-11 10:56:46 -0500

    working saved colleges page per user uid

--- study-easy-react/src/profile/savedColleges.js
+++ study-easy-react/src/profile/savedColleges.js
@@ -3,12 +3,43 @@ import { addDoc, collection, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db, auth } from "../firbase-config";
 
-export default function savedCollegesProfile()  {
+export default function SavedCollegesProfile()  {
+
+    const collectionRef = collection(db, "savedColleges");
+
+    const [userProfile, setUserProfile] = useState();
+
+    const openInNewTab = url => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+      };
+
+    useEffect(() => {
+        const getCurrentUserProfile = async () => {
+          const data = await getDocs(collectionRef);
+          console.log(data, " dat from useEffect");
+          setUserProfile(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+          console.log(userProfile, " Only userasdasfasd");
+        }
+        getCurrentUserProfile();
+    },[])
+
+
 
 
     return (
         <div>
-            Saved Colleges
+            {userProfile?.map((e, index) => {
+                let inComeingUid = e.uid;
+                let currentUidofUser = auth.currentUser.uid;
+                if (inComeingUid === currentUidofUser) {
+                return (
+                    <div key={index}>
+                        <p>
+                            {e?.CollegeName} <Button onClick={() => openInNewTab(e?.url)}>College home page</Button> 
+                        </p>
+                    </div>
+                )}
+            })}
         </div>
     )
 }
