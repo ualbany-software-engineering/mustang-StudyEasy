7313656d19e76e8cad71237fb31ba77a724dfe70
anudeep-17 <anudeepsai88@gmail.com>
2022-11-17 18:56:43 -0500

    messed

--- study-easy-react/src/firbase-config.js
+++ study-easy-react/src/firbase-config.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import {getAnalytics} from "firebase/analytics";
 
 
 const firebaseConfig = {
@@ -20,3 +21,4 @@ const firebaseConfig = {
   export const auth = getAuth(app);
   export const db = getFirestore(app);
   export const storage=getStorage(app);
+  export const analytics = getAnalytics(app);
