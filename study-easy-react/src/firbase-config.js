281c093c054196152cb7a5174d331c5ad1f12d8c
anudeep-17 <anudeepsai88@gmail.com>
2022-11-17 18:48:54 -0500

    taar maar

--- study-easy-react/src/firbase-config.js
+++ study-easy-react/src/firbase-config.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyCnShKPKieY-ozV82CqAH4pzN87aYCf41U",
@@ -19,5 +19,4 @@ const firebaseConfig = {
   export const app = initializeApp(firebaseConfig);
   export const auth = getAuth(app);
   export const db = getFirestore(app);
-  export const analytics = getAnalytics(app);
   export const storage=getStorage(app);
